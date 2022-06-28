eda3bafb19ccad4ab06adc11fc055fc4d352ea1b
gaston segui <seguigastonmail.com>
2022-06-28 15:09:02 -0300

    test

--- Tests.py
+++ Tests.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 import selenium
 from selenium import webdriver
@@ -47,6 +48,7 @@ except TimeoutException:
     quit()
 
 patologia = driver.find_elements_by_xpath("/html/body/div[3]/div[1]")
+
 #Paso la data de la patologia a un diccionario
 drogas = []
 for a in patologia:
@@ -72,6 +74,7 @@ file.close()
 #WebScraping Vademecum
 driver = webdriver.Chrome(executable_path=r"C:\Users\segui\Downloads\chromedriver.exe")
 driver.get(website)
+
 def keys_2(XPATH):
     WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.XPATH, XPATH))) \
@@ -110,10 +113,12 @@ for i in range(10):
 df_data = pd.DataFrame(farmaco)
 df_data.to_csv("Vademecum.csv")
 driver.quit()
+
 #Armo una lista con split()
 with open("Tratamiento.txt", "r") as f:
     for line in f:
         x = line.strip().split()
+
 #Ordeno y depuro el archivo de texto
 def remove_punc(string):
     punt = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
@@ -123,11 +128,14 @@ def remove_punc(string):
     return string
 x = [remove_punc(i) for i in x]
 
+#Rescato los precios de los farmacos
+
 if input_2 in x:
     print(f"{input_2}, se encuentra en la base de datos")
     print(df_data)
 elif input_2 not in x:
     print(f"{input_2} no coincide con la base de datos")
+
 #Visualizacion de datos
 data_base = pd.read_csv("Vademecum.csv")
 data_base.to_csv("sample.csv", index=False, header=False, mode= "a")
--- tes.py
+++ tes.py
@@ -15,6 +15,8 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium.common.exceptions import ElementNotSelectableException
+import statistics
+from statistics import mean
 #URLs
 website = "https://servicios.pami.org.ar/vademecum/views/consultaPublica/listado.zul"
 
@@ -24,55 +26,9 @@ input_2 = input("ingrese su farmaco: ")
 #WebScraping Vademecum
 driver = webdriver.Chrome(executable_path=r"C:\Users\segui\Downloads\chromedriver.exe")
 driver.get(website)
-b = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i")))
-a = driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-try:
-    droga = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#zk_comp_28"))) \
-        .send_keys(input_2)
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-default.btn-sm.btn"))) \
-        .click()
 
-
-except TimeoutException:
-    print("Tiempo de espera terminado. Reinicie el programa.")
-    driver.quit()
-i = []
-while True:
+def scrap_precios():
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i")))\
-        .click()
-    time.sleep(5)
-else:
-    driver.quit()
-
-
-"""def prueba():
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i"))) \
-        .click()
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    precio = driver.find_elements_by_xpath(
-        "/html/body/div[1]/div/div/div/div[2]/div/div/div[2]/div[3]/table/tbody[1]/tr/td[7]/span")
-    print(precio)
-for i in range(4):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    prueba()
-else:
-    pass"""
-
-"""i = []
-
-while i in range(3):
-    WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i"))) \
-        .click()
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
-        (By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div/div[2]/div[3]/table/tbody[1]/tr/td[7]/span")))
-    WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i"))) \
-        .click()
     precio = driver.find_elements_by_xpath(
         "/html/body/div[1]/div/div/div/div[2]/div/div/div[2]/div[3]/table/tbody[1]/tr/td[7]/span")
     precios = []
@@ -80,14 +36,51 @@ while i in range(3):
         precios.append(a.text)
     precios = [ele for ele in precios if ele.strip()]
     farmaco = []
-    for i in range(10):
-        data = {"Precio": precios[i]}
+    for i in range(len(precios)):
+        data = {"Precio": precios[i].strip("$"),
+                }
         farmaco.append(data)
     df_data = pd.DataFrame(farmaco)
     print(df_data)
-    df_data.to_csv("Precios.csv")"""
+    df_data.to_csv("Precios.csv")
+    data_base = pd.read_csv("Precios.csv")
+    data_base.to_csv(f"{input_2}.csv", index=False, header=False, mode="a")
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i"))) \
+        .click()
+    time.sleep(15)
+
+try:
+    droga = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#zk_comp_28"))) \
+        .send_keys(input_2)
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-default.btn-sm.btn"))) \
+        .click()
+
+except TimeoutException:
+    print("Tiempo de espera terminado. Reinicie el programa.")
+    driver.quit()
 
 
+while True:
+    scrap_precios()
+    element = driver.find_element_by_xpath('//a[@name="zk_comp_99-next"]').get_attribute("disabled")
+    if element == None:
+        continue
+    else:
+        scrap_precios()
+
+        time.sleep(30)
+        driver.quit()
+        quit()
+quit()
+with open(f"{input_2}.csv", "r"):
+    file = pd.DataFrame(f"{input_2}.csv")
+    print(file.head())
+    print(file["Precio"].mean())
+"""print(pd.DataFrame(f"{input_2}.csv").mean(axis=0))
+with open(f"{input_2}.csv", "r") as f:
+    print(f.mean(axis=0))
+"""
 
 
 
